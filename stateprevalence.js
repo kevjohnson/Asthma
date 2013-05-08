130c42f5750d57b697bcc553d6d32c12638006d7
elsherbini <joseph.elsherbini@gmail.com>
2013-05-07 21:41:25 -0300

    shit-ton of changes

--- stateprevalence.js
+++ stateprevalence.js
@@ -1,97 +1,147 @@
- 
-var width = 960,
-    height = 500;
+var width = 1000,
+	height = 500,
+	names = {},
+	path = d3.geo.path();
 
-var quantizeresp = d3.scale.quantize()
-    .domain([.04, .17])
-    .range(d3.range(9).map(function(i) { return "q" + i + "-9"; }));
-	
-var quantizepred = d3.scale.quantize()
-    .domain([.07, .13])
-    .range(d3.range(9).map(function(i) { return "q" + i + "-9"; }));
-	
-var quantizediff = d3.scale.quantize()
-    .domain([0, .04])
-    .range(d3.range(9).map(function(i) { return "q" + i + "-9"; }));
- 
-var path = d3.geo.path();
- 
+var quantize = d3.scale.quantize()
+	.range(d3.range(9).map(function(i) { return "q" + i + "-9"; }));
+
 var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+	.attr("width", width)
+	.attr("height", height);
  
 queue()
-    .defer(d3.json, "us-10m.json")
-    .defer(d3.csv, "state-choropleth.csv")
+  .defer(d3.json, "us-10m.json")
+  .defer(d3.csv, "state-choropleth.csv")
 	.defer(d3.tsv, "us-state-names.tsv")
-    .await(ready);
+  .await(ready);
   
 
 function ready(error, us, asthmadata, statenames) {
   var responseData = {};
   var predictedData = {};
   var differenceData = {};
-  
+  var stateData = {};
+  var dataArray=new Array(3);
 
-  
-  asthmadata.forEach(function(d) { responseData[d.State] = +d.Response; });
-  asthmadata.forEach(function(d) { predictedData[d.State] = +d.Predicted; });
-  asthmadata.forEach(function(d) { differenceData[d.State] = +d.Difference; });
+//embarassing hack. there has got to be a better way to do this.
+//loop to create an array (dataArray) that has 3 arrays, each representing one of the parameters for ach state (predicted, response, difference)   
+for (idx=0;idx<3;idx++){
 
-  var select = d3.select("body").append("select");
+  dataArray[idx]= new Array(51);
+  asthmadata.forEach(function(d,i){  
+    responseData[d.State] = +d.Response;
+    predictedData[d.State] = +d.Predicted;
+    differenceData[d.State] = +d.Difference;
+    stateData[d.State]=[responseData[d.State],predictedData[d.State],differenceData[d.State]];    
+    dataArray[idx][i]={State: d.State, value: stateData[d.State][idx]};
   
+	});
+}
+//make a dropdown menu with the parameters as options
+  var select = d3.select("body").append("select");  
 
   var parameters = ["Response","Predicted","Difference"];
   select.selectAll("option")
-	.data(parameters)
-	.enter().append("option")
-	.attr("value", function(d){return d;})
-	.text(function(d){return d;});
+		.data(parameters)
+	 .enter().append("option")
+		.attr("value", function(d,i){return i;})
+		.text(function(d){return d;});
 
   select.on("change",function(){
-	var parameter = parameters[this.selectedIndex];
-	updateMap(parameter);	
+		var parameter = this.selectedIndex; //this.selectedIndex returns a number which corresponds to the parameter array above
+		updateMap(parameter);
 	});
-  var g = svg.append("g")
-	.attr("class", "states")
   
-  updateMap("Response");
+	var g = svg.append("g")
+		.attr("class", "states")
+  
+  updateMap(0);
 
   function updateMap(parameter){
-	  var names = {};
+
+	  var r;
 	  statenames.forEach(function(d){
 		  names[d.id] = d.name;
 		});
-	  asthmadata.forEach(function(d){
-		if(parameter=="Response"){r = d.Response*100;}
-		if(parameter=="Predicted"){r = d.Predicted*100;}
-		if(parameter=="Difference"){r = d.Difference*100;}
-		names[d.State] = names[d.State] + ": " + r.toFixed(1) + "%";
+	  
+		asthmadata.forEach(function(d,i){
+			r=dataArray[parameter][i].value*100; //no more if/then statements!
+			names[d.State] = names[d.State] + ": " + r.toFixed(1) + "%";
 	  });
 
+//change the domain of the quantize function to match the min and max of the data for the given parameter
+	quantize.domain(
+		[d3.min(dataArray[parameter], function(d,i){return dataArray[parameter][i].value;}),
+		d3.max(dataArray[parameter], function(d,i){return dataArray[parameter][i].value;})]
+	)
+
 	g.selectAll("path").remove();
-    var paths = g.selectAll("path")
-      .data(topojson.object(us, us.objects.states).geometries)
-    .enter().append("path")
-      .attr("class", function(d) {
-	  if(parameter==="Response"){
-		
-		return quantizeresp(responseData[d.id]);
-	  
-	  } else if(parameter==="Predicted"){
-		
-		return quantizepred(predictedData[d.id]);
-		
-	  } else{
-		
-		return quantizediff(differenceData[d.id])};
-	  
-	  })
 
+	var paths = g.selectAll("path")
+		.data(topojson.object(us, us.objects.states).geometries)
+	 .enter().append("path")
+		.attr("class", function(d,i){return quantize( dataArray[parameter][i].value );})
+		.attr("d", path)
+		.on("mouseover",showCaption)
+		.on("mouseout",function(){caption.html(starter);});
+	
+	updateLegend(parameter);
+  }; 
+  
+  function updateLegend(parameter){
 
-      .attr("d", path)
-	  .on("mouseover",showCaption)
-	  .on("mouseout",function(){caption.html(starter);});
+		//compute the min-max range for each quanta, and store the results in an array
+		var quanta = quantize.range().length;
+		var quantaValues = new Array(quanta);
+		for (idx=0;idx<quanta;idx++){
+			quantaValues[idx] = {
+				min: (quantize.domain()[0] + idx*((quantize.domain()[1] - quantize.domain()[0])/(quanta-1))),
+				max: (quantize.domain()[0] + (idx+1)*((quantize.domain()[1] - quantize.domain()[0])/(quanta-1)))
+			}
+		}
+		
+		//clear the legend (I think there is a better way to do this with .exit()
+		svg.selectAll(".legend").remove();
+	
+		//draw the new legend by appending "quanta" number of g elements, each translated 25px down from the last
+		var legend = svg.selectAll(".legend");
+		
+		legend.data(quantaValues)
+		 .enter().append("g")
+			.attr("class", "legend")
+			.attr("transform", function(d, i) {return "translate(0," + ((i * 25) + 25) + ")"; });
+			
+		//draw the boxes in each g, and give them the correct class to turn the right color
+     svg.selectAll(".legend").append("rect")
+      .attr("x", width - 18)
+      .attr("width", 18)
+      .attr("height", 18)
+      .attr("class", function(d){return quantize(d.min);})
+			.style("stroke","#000");
+		
+		//append the text labels to the g elements
+    svg.selectAll(".legend").append("text")
+			.attr("x", width - 21)
+			.attr("y", 9)
+			.attr("dy", ".4em")
+			.attr("font-family", "sans-serif")
+      .style("text-anchor", "end")
+			.style("fill", "#000")
+      .text(function(d) { return (d.min*100).toFixed(1) + "% - " + (d.max*100).toFixed(1) + "%"; });
+			
+		// change the legend title depending on the parameter being displayed
+		svg.append("text")
+			.classed("legend", true) //class as legend so it gets removed on update
+			.attr("transform", "translate(0,12)")
+			.attr("x",width-18)
+			.attr("y", 0)
+			.attr("dy", ".4em")
+			.attr("font-family", "sans-serif")
+			.style("text-anchor", "end")
+			.text(parameters[parameter]);
+  };
+  
   var caption = d3.select('#caption')
   ,starter = caption.html();
 
@@ -99,5 +149,4 @@ function ready(error, us, asthmadata, statenames) {
     var name = names[d.id];
     caption.html(name);
   }
-}
 }
